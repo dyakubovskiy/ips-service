c3a98e06cd12825538372b478588006e66504c17
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-09-19 22:14:47 +0300

    Добавил CenteredLayout

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import { CenteredLayout } from '../layout'
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/auth',
+      name: 'auth',
+      children: [],
+      component: CenteredLayout,
+    },
+  ],
 })
 
 export default router
