e7bc0f0d71c622c95c656d143131e4576e4d475e
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-09-21 21:58:59 +0300

    Добавил карточку тарифов

---
+++ src/pages/main/ui/TariffCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <div class="hstack">
+      <div class="info">
+        <h2>{{ name }}</h2>
+        <div
+          v-if="desc"
+          class="small">
+          {{ desc }}
+        </div>
+      </div>
+      <div class="vstack">
+        <div
+          v-if="price"
+          class="price">
+          {{ currencyFormatter.format(price) }}
+        </div>
+        <div
+          v-if="speed"
+          class="speed">
+          {{ speed }} Mb/s
+        </div>
+        <RouterLink to="/">Подробнее</RouterLink>
+        <button
+          type="button"
+          class="btn btn-primary">
+          Подключить
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Tariff } from '../api'
+
+defineProps<Tariff>()
+
+const currencyFormatter = new Intl.NumberFormat('ru-RU', {
+  style: 'currency',
+  currency: 'RUB'
+})
+</script>
+
+<style scoped>
+.hstack {
+  justify-content: space-between;
+}
+</style>
---
+++ src/pages/main/ui/TariffSkeleton.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="hstack">
+      <div class="info">
+        <div class="name skeleton" />
+        <div class="desc skeleton" />
+        <div class="desc skeleton" />
+      </div>
+      <div class="vstack">
+        <div class="price skeleton" />
+        <div class="speed skeleton" />
+        <div class="link skeleton" />
+        <div class="button skeleton" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+}
+
+.name {
+  height: 2.4rem;
+  width: 60%;
+}
+
+.desc {
+  height: 3.4rem;
+  width: 100%;
+}
+
+.price,
+.speed,
+.link {
+  width: 12rem;
+  height: 2rem;
+}
+
+.button {
+  width: 12rem;
+  height: 3.6rem;
+}
+</style>
