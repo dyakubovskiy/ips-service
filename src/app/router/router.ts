0124e49ecaa94724c2fbb4f81df70b338f58a833
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-12-26 22:52:58 +0300

    Добавил редирект с несуществующих ссылок

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -31,6 +31,10 @@ const router = createRouter({
         PROFILE_ROUTE
       ],
       component: SidebarLayout
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: MAIN_LINK
     }
   ]
 })
